a69d56e772ba52d82b4d8cf521e4be7f82c96cbe
Eastyn <eastynwork@gmail.com>
2024-09-13 12:43:01 +0900

    create getJoin and getLogin and the routes

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,3 +1,7 @@
 import User from "../models/User";
 
 export const home = async (req, res) => res.render("home", { pageTitle: "A13 User" });
+
+export const getJoin = (req, res) => res.render("join", { pageTitle: "Join us" });
+
+export const getLogin = (req, res) => res.render("login", { pageTitle: "Login" });
--- src/router/userRouter.js
+++ src/router/userRouter.js
@@ -1,8 +1,10 @@
 import express from "express";
-import { home } from "../controller/userController";
+import { getJoin, getLogin, home } from "../controller/userController";
 
 const userRouter = express.Router();
 
 userRouter.get("/", home);
+userRouter.route("/join").get(getJoin);
+userRouter.route("/login").get(getLogin);
 
 export default userRouter;
